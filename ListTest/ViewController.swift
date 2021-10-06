00e72c00ac69c1203fe9f8c75059311160572777
Artemis Shlesberg <temitrix@gmail.com>
2021-10-06 14:32:01 +0300

    Initial Commit

---
+++ ListTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListTest
+//
+//  Created by Артeмий Шлесберг on 06.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
